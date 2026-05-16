59f70186141c3298ac8ce2f23d05408c9bdb1db5
sebascoco <s.salazars2@uniandes.edu.co>
2026-05-15 19:37:21 -0500

    pruebasss

--- main.py
+++ main.py
@@ -13,16 +13,9 @@ app.add_middleware(
     allow_headers=["*"]
 )
 
-# os.environ para despliegue. Descomente cuando ya probó todo local.
 client = MongoClient(os.environ["MONGO_URI"])
-# TODO: conectarse al cluster Admonsis  
-# client = MongoClient("mongodb://ISIS2304C29202610:OE6wZSWPzb54@157.253.236.88:8087")
-
-# client = MongoClient("mongodb://ISIS2304C29202610:OE6wZSWPzb54@157.253.236.88:8087")
-# TODO: conectarse a la base de datos Admonsis  
-# db = client["ISIS*******"]
-db = client["ISIS2304"]
-
+db_propia = client["ISIS2304C29202610"]  # escritura: comentarios y eventos
+db_bares  = client["ISIS2304"]           # lectura: bares y bebedores
 
 @app.get("/")
 def inicio():
@@ -30,46 +23,29 @@ def inicio():
 
 @app.get('/bares/{bar_id}/comentarios')
 def get_comentarios(bar_id: int):
-    comentarios = list(db["comentarios_bares"].find({"bar_id": bar_id}, {"_id": 0}))
+    comentarios = list(db_propia["comentarios_bares"].find({"bar_id": bar_id}, {"_id": 0}))
     return comentarios
 
 @app.post('/bares/{bar_id}/comentarios')
 def post_comentario(bar_id: int, datos: dict):
     datos['bar_id'] = bar_id
-    datos['fecha']  = datetime.now().isoformat()
-    db["comentarios_bares"].insert_one(datos)
+    datos['fecha'] = datetime.now().isoformat()
+    db_propia["comentarios_bares"].insert_one(datos)
     return {'mensaje': 'Comentario guardado'}
 
-# TODO: implementar GET /bares/{bar_id}/eventos
-# Debe retornar todos los eventos del bar desde la colección 'eventos'
 @app.get('/bares/{bar_id}/eventos')
 def get_eventos(bar_id: int):
-    eventos = list(db["eventos"].find({"bar_id": bar_id}, {"_id": 0}))
+    eventos = list(db_propia["eventos"].find({"bar_id": bar_id}, {"_id": 0}))
     return eventos
 
-# TODO: implementar POST /bares/{bar_id}/eventos  
-# Debe insertar el evento en la colección 'eventos'
-# Recuerde agregar bar_id y fecha_creacion al documento antes de insertar
 @app.post('/bares/{bar_id}/eventos')
 def post_evento(bar_id: int, datos: dict):
     datos['bar_id'] = bar_id
     datos['fecha_creacion'] = datetime.now().isoformat()
-    db["eventos"].insert_one(datos)
+    db_propia["eventos"].insert_one(datos)
     return {'mensaje': 'Evento guardado'}
 
 @app.get('/bares/{bar_id}')
 def get_bares(bar_id: int):
-    bar = db["Bares"].find_one({"_id": bar_id}, {"_id": 0})
+    bar = db_bares["Bares"].find_one({"_id": bar_id}, {"_id": 0})
     return bar
-
-@app.get('/test')
-def test_conexion():
-    try:
-        colecciones = db.list_collection_names()
-        bar = db["Bares"].find_one({"_id": 58}, {"_id": 0})
-        return {
-            "colecciones": colecciones,
-            "bar_58": bar
-        }
-    except Exception as e:
-        return {"error": str(e)}
